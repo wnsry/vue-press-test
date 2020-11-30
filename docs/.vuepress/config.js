2d30d8123e1fabc7647c872a4ab2ce34bb7f06ad
dingke <891862008@qq.com>
2020-11-30 18:37:47 +0800

    yi

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Hello VuePress1',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'VuePress', link: 'https://vuepress.vuejs.org/' },
+    ]
+  }
+}
